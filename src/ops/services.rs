c0bd79aa456b9fc86506f56418e644c437a97a63
Leonhard Markert <curiousleo@users.noreply.github.com>
2019-12-05 16:53:00 +0100

    cleanup

--- src/ops/services.rs
+++ src/ops/services.rs
@@ -3,14 +3,14 @@
 use crate::ops::error::{ok, ExitError, OpResult};
 use futures::prelude::*;
 use slog_scope::{info, warn};
-use std::path::{Path, PathBuf};
 use std::fs::File;
+use std::path::{Path, PathBuf};
 use std::process::Stdio;
 use tokio::io::AsyncBufReadExt;
 use tokio::io::BufReader;
 use tokio::process::Command;
 use tokio::runtime::Runtime;
-use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::sync::mpsc::{channel, Receiver};
 
 #[derive(Copy, Clone, Debug)]
 enum Fd {
@@ -34,10 +34,11 @@ struct Service {
 
 /// See the documentation for lorri::cli::Command::Services.
 pub fn main(config: &Path) -> OpResult {
-    let services: Vec<Service> = match serde_json::from_reader(std::io::BufReader::new(File::open(config)?)) {
-        Ok(services) => services,
-        Err(e) => Err(ExitError::temporary(format!("{}", e)))?,
-    };
+    let services: Vec<Service> =
+        match serde_json::from_reader(std::io::BufReader::new(File::open(config)?)) {
+            Ok(services) => services,
+            Err(e) => Err(ExitError::temporary(format!("{}", e)))?,
+        };
     Runtime::new()?.block_on(main_async(services));
     ok()
 }
@@ -51,11 +52,10 @@ async fn main_async(services: Vec<Service>) {
     tokio::spawn(start_services(service_rx)).await.unwrap()
 }
 
-async fn to_log<'a, L: Stream<Item = tokio::io::Result<String>> + std::marker::Unpin>(
-    mut lines: L,
-    name: String,
-    fd: Fd,
-) {
+async fn log_stream<'a, L>(mut lines: L, name: String, fd: Fd)
+where
+    L: Stream<Item = tokio::io::Result<String>> + std::marker::Unpin,
+{
     while let Some(Ok(message)) = lines.next().await {
         match fd {
             Fd::Stdout => info!("{}", message; "name" => &name),
@@ -73,12 +73,12 @@ async fn start_services(mut service_rx: Receiver<Service>) {
             .stderr(Stdio::piped())
             .spawn()
             .unwrap();
-        tokio::spawn(to_log(
+        tokio::spawn(log_stream(
             BufReader::new(child.stdout().take().unwrap()).lines(),
             service.name.to_string(),
             Fd::Stdout,
         ));
-        tokio::spawn(to_log(
+        tokio::spawn(log_stream(
             BufReader::new(child.stderr().take().unwrap()).lines(),
             service.name.to_string(),
             Fd::Stderr,
@@ -86,23 +86,3 @@ async fn start_services(mut service_rx: Receiver<Service>) {
     }
 }
 
-// TESTING ONLY
-async fn spawner(mut service_tx: Sender<Service>) {
-    let mut id: u64 = 0;
-    let duration = std::time::Duration::from_millis(1000);
-    loop {
-        tokio::time::delay_for(duration).await;
-        let name = format!("echo {}", id);
-        id += 1;
-        service_tx
-            .send(Service {
-                name,
-                path: PathBuf::from(
-                    "/nix/store/fa4zygrvfq77gccqiyl9kixs05nfihk1-bash-interactive-4.4-p23/bin/bash",
-                ),
-                args: vec!["-c".to_string(), "echo start; sleep 2; echo hi; sleep 2; echo bye >&2".to_string()],
-            })
-            .await
-            .unwrap();
-    }
-}
